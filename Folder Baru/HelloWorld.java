049830818118f73abe63a00d82a730b20fa7be8c
dmarsenp <dmarsenp@student.telkomuniversity.ac.id>
2020-09-17 14:06:35 +0700

    HelloWorld.java

---
+++ Folder Baru/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main (String[] args) {
+		System.out.println("Nama : David Marsem Purba!");
+		System.out.println("NIM : D3IF6706201142!");
+		System.out.println("Kelas : 44-03!");		
+		}
+	} 
